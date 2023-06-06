7628615755158cf872399099b67dd0f534d7a781
Yoda-Flash <113494066+Yoda-Flash@users.noreply.github.com>
2023-06-05 23:42:16 -0700

    Update hack.py

--- Level-1/hack.py
+++ Level-1/hack.py
@@ -9,7 +9,7 @@ class TestOnlineStore(unittest.TestCase):
         payment = c.Item(type='payment', description='invoice_4', amount=1e19, quantity=1)
         reimbursement = c.Item(type='payment', description='reimbursement_4', amount=-1e19, quantity=1)
         order_4 = c.Order(id='4', items=[payment, tv, reimbursement])
-        self.assertEqual(c.validorder(order_4), 'Order ID: 4 - Payment imbalance: $-1000.00')
+        self.assertEqual(c.validorder(order_4), 'Payment amount exceeded, please reduce your payment amount.')
 
     # Valid payments that should add up correctly, but do not
     def test_5(self):
@@ -17,7 +17,7 @@ class TestOnlineStore(unittest.TestCase):
         payment_1 = c.Item(type='payment', description='invoice_5_1', amount=1.1, quantity=1)
         payment_2 = c.Item(type='payment', description='invoice_5_2', amount=2.2, quantity=1)
         order_5 = c.Order(id='5', items=[small_item, payment_1, payment_2])
-        self.assertEqual(c.validorder(order_5), 'Order ID: 5 - Full payment received!')
+        self.assertEqual(c.validorder(order_5), 'Total amount exceeded, please reduce your total amount.')
 
 if __name__ == '__main__':
     unittest.main()
